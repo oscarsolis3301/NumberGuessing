3f4b0cf03d2999c41aeb5dd718473bed8d2a0259
Oscar Solis <oscarsolis3301@gmail.com>
2022-04-06 10:42:49 -0600

    Checking if user runs out of lives

--- Game.py
+++ Game.py
@@ -10,8 +10,9 @@ random_number = random.randint(1, 100)
 
 
 def game_loop(lives):
+    """Main game loop that takes in lives as a parameter"""
     while lives != 0:
-        print(f"You have {lives} remaining to guess the number.")
+        print(f"You have {lives} attempts remaining to guess the number.")
         user_guess = int(input("Make a guess: "))
         if user_guess != random_number:
             lives -= 1
@@ -21,12 +22,12 @@ def game_loop(lives):
                 print("Too low.")
             print("Guess again.")
         elif user_guess == random_number:
-            print("You have guessed the number!")
+            print(f"You got it! The answer was {user_guess}")
             break
-
+    print("You've run out of guesses, you lose.")    
 if input("Choose a difficulty. Type 'easy' or 'hard': ") == "easy":
     lives = 10
     game_loop(lives)
-elif input("Choose a difficulty. Type 'easy' or 'hard': ") == "hard":
+else:
     lives = 5
     game_loop(lives)
